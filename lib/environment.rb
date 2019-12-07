28cd9f9d848673d16b0e3619c8f7fb4188d3eef9
nicmlu <NicoleMLucien@gmail.com>
2019-12-07 11:26:22 -0500

    update require relative file paths

--- lib/environment.rb
+++ lib/environment.rb
@@ -1,10 +1,10 @@
 require 'httparty'
 require 'pry'
-require_relative "world_cusine/version"
-require_relative "world_cusine/api"
-require_relative "world_cusine/area"
-require_relative "world_cusine/cli"
-require_relative "world_cusine/meal"
+require_relative "./world_cusine/version"
+require_relative "./world_cusine/api"
+require_relative "./world_cusine/area"
+require_relative "./world_cusine/cli"
+require_relative "./world_cusine/meal"
 
 module World_Cusine
   class Error < StandardError; end
