8cf32b7acfe267665d53df17efedd1987c20d9c2
nicmlu <NicoleMLucien@gmail.com>
2019-12-07 12:43:57 -0500

    update meal info method to include conditional that puts ingredient if not null

--- lib/world_cusine/cli.rb
+++ lib/world_cusine/cli.rb
@@ -64,36 +64,51 @@ class World_Cusine::CLI
 
         puts "To see more information about a meal, type in the number of that meal or type in exit to end the search..."
         
-        
         input = gets.chomp 
-        if input === "exit"
+        if input == "exit"
         goodbye
-        elsif
-    
-        selected_meal = World_Cusine::Meal.get_meal_info
-        meal_ingredients = World_Cusine::Meal.meal_ingredients
-
+        elsif (1..World_Cusine::Meal.all.size).include?(input.to_i)
+        
         puts "Here is the meal you selected:"
         puts "=============================================================="
+    
+        selected_meal = World_Cusine::Meal.all[input.to_i - 1]
+        World_Cusine::Meal.get_meal_info(selected_meal)
 
         puts "Meal Name: #{Meal.strMeal}"
         puts "Meal Category: #{Meal.strCategory}"
         puts "Meal Area: #{Meal.strArea}"
 
         puts "=============================================================="
-        puts "Meal Ingredients:"
-        meal_ingredients 
+        puts "Meal Ingredients: "
+        puts "#{strIngredient1}: #{strMeasure1}" unless strIngredient1 == "null" || strIngredient1 == ""
+        puts "#{strIngredient2}: #{strMeasure2}" unless strIngredient2 == "null" || strIngredient2 == ""
+        puts "#{strIngredient3}: #{strMeasure3}" unless strIngredient3 == "null" || strIngredient3 == ""
+        puts "#{strIngredient4}: #{strMeasure4}" unless strIngredient4 == "null" || strIngredient4 == ""
+        puts "#{strIngredient5}: #{strMeasure5}" unless strIngredient5 == "null" || strIngredient5 == ""
+        puts "#{strIngredient6}: #{strMeasure6}" unless strIngredient6 == "null" || strIngredient6 == ""
+        puts "#{strIngredient7}: #{strMeasure7}" unless strIngredient7 == "null" || strIngredient7 == ""
+        puts "#{strIngredient8}: #{strMeasure8}" unless strIngredient8 == "null" || strIngredient8 == ""
+        puts "#{strIngredient9}: #{strMeasure9}" unless strIngredient9 == "null" || strIngredient9 == ""
+        puts "#{strIngredient10}: #{strMeasure10}" unless strIngredient10 == "null" || strIngredient10 == ""
+        puts "#{strIngredient11}: #{strMeasure11}" unless strIngredient11 == "null" || strIngredient11 == ""
+        puts "#{strIngredient12}: #{strMeasure12}" unless strIngredient12 == "null" || strIngredient12 == ""
+        puts "#{strIngredient13}: #{strMeasure13}" unless strIngredient13 == "null" || strIngredient13 == ""
+        puts "#{strIngredient14}: #{strMeasure14}" unless strIngredient14 == "null" || strIngredient14 == ""
+        puts "#{strIngredient15}: #{strMeasure15}" unless strIngredient15 == "null" || strIngredient15 == ""
+        puts "#{strIngredient16}: #{strMeasure16}" unless strIngredient16 == "null" || strIngredient16 == ""
+        puts "#{strIngredient17}: #{strMeasure17}" unless strIngredient17 == "null" || strIngredient17 == ""
+        puts "#{strIngredient18}: #{strMeasure18}" unless strIngredient18 == "null" || strIngredient18 == ""
+        puts "#{strIngredient19}: #{strMeasure19}" unless strIngredient19 == "null" || strIngredient19 == ""
+        puts "#{strIngredient20}: #{strMeasure20}" unless strIngredient20 == "null" || strIngredient20 == ""
 
+        puts "=============================================================="
         puts "Meal Instructions: #{Meal.strInstructions}"
 
         else 
             puts "Sorry that is not a valid option, please try a different number"
             list_meals
         end
-
-        puts ""
-        
-    
     end 
 
     # def another_search
