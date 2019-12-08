3308d3555ad272bbd86d08872e15bd0497e01049
nicmlu <NicoleMLucien@gmail.com>
2019-12-07 21:24:42 -0500

    correct class name spelling

--- lib/world_cusine/api.rb
+++ lib/world_cusine/api.rb
@@ -1,14 +1,14 @@
-class World_Cusine::API
+class World_Cuisine::API
   attr_accessor :strArea, :idMeal
 
   def self.all_areas
     area_response = HTTParty.get('https://www.themealdb.com/api/json/v1/1/list.php?a=list')
-    area_response["meals"].each {|area_hash| World_Cusine::Area.new(area_hash)}
+    area_response["meals"].each {|area_hash| World_Cuisine::Area.new(area_hash)}
   end
 
   def self.get_area_meals(area_instance)
     meals_response = HTTParty.get("https://www.themealdb.com/api/json/v1/1/filter.php?a=#{area_instance.strArea}")
-    meals_response["meals"].each {|meal_hash| World_Cusine::Meal.new(meal_hash)}
+    meals_response["meals"].each {|meal_hash| World_Cuisine::Meal.new(meal_hash)}
   end
 
   def self.get_meal_info(meal_instance)
